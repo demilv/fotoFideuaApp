35048e8009d77988608c8b70de58935cd8a88e00
demilv <gonzalo.cano@bootcamp-upgrade.com>
2024-10-25 10:14:52 +0200

    Cambios adicionales al css

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -26,38 +26,20 @@ const OrderOptions = () => {
         <>
             {location.pathname === "/favoritos" ? (
                 <div className="orderOptionsContainer">
-                    <h3 className="navInstrucciones">¿Quieres cambiar el orden? Tú eliges</h3>                        
+                    <h3 className="navInstrucciones">Ordena tus fotos</h3>                        
                     <ul className="navOpciones">
                         <div className="opcionesG">
-                            <li className="opcionesGleft1">
-                                <input className="opcionesRadio" type="radio" name="ordenar" value="width" onChange={handleOrderChange} />
-                                <label htmlFor="width">Anchura</label>
-                            </li>
-                            <li>
-                                <input className="opcionesRadio" type="radio" name="ordenar" value="created" onChange={handleOrderChange} />
-                                <label htmlFor="created">Fecha</label>
-                            </li>
-                        </div>
-                        <div className="opcionesG">
-                            <li className="opcionesGleft2">
-                                <input className="opcionesRadio" type="radio" name="ordenar" value="height" onChange={handleOrderChange} />
-                                <label htmlFor="height">Altura</label>
-                            </li>
-                            <li>
-                                <input className="opcionesRadio" type="radio" name="ordenar" value="likes" onChange={handleOrderChange} />
-                                <label htmlFor="likes">Likes</label>
-                            </li>
-                        </div>
-                        <div className="opcionesG">
-                            <li className="opcionesGleft3">
-                                <input className="opcionesRadio" type="radio" name="ordenar" value="" onChange={handleOrderChange} />
-                                <label htmlFor="height">No deseo aplicar ningún orden</label>
-                            </li>
+                            <select className="opcionesSelect" name="ordenar" id="ordenar" onChange={handleOrderChange}>
+                                <option value="width">Anchura</option>
+                                <option value="created">Fecha</option>
+                                <option value="height">Altura</option>
+                                <option value="likes">Likes</option>
+                                <option value="">Sin orden</option>
+                            </select>
                         </div>
                     </ul>
                 </div>) :  
                 (<div className="orderOptionsContainer">
-                    <h3 className="navInstrucciones">Bienvenido a nuestra App de fotos! Clica la estrella de aquellas que quieras guardar en favorios!</h3>
                 </div>)
             }
         </>
